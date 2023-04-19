22fb1bed36dc83da6fdf84cbd77fbb2ff0e192dd
AlexRodriguez22 <106100369+AlexRodriguez22@users.noreply.github.com>
2023-04-18 22:46:54 -0400

    Delete MakerspaceApp/js directory

--- MakerspaceApp/js/scripts.js
+++
@@ -1,7 +0,0 @@
-function toggleContent(button) {
-  const targetId = button.getAttribute('data-target');
-  const targetElement = document.getElementById(targetId);
-  const isHidden = targetElement.style.display === 'none';
-
-  targetElement.style.display = isHidden ? 'block' : 'none';
-}
